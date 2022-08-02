235f2c27f7d1a3902e141b6aa7223806199557c7
Wendell <wendellsri2@hotmail.com>
2022-08-02 17:23:41 -0300

    Criando o repositorio

---
+++ EscolaAngular-WebApi/Data/Repositorio.cs
@@ -0,0 +1,141 @@
+﻿using EscolaAngular_WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EscolaAngular_WebApi.Data
+{
+    public class Repositorio : IRepositorio
+    {
+        private readonly DataContext _context;
+
+        public Repositorio(DataContext context)
+        {
+            _context = context;
+        }
+        public void Add<T>(T entity) where T : class
+        {
+            _ = _context.Add(entity);
+        }
+        public void Update<T>(T entity) where T : class
+        {
+            _ = _context.Update(entity);
+        }
+        public void Delete<T>(T entity) where T : class
+        {
+            _ = _context.Remove(entity);
+        }
+        public async Task<bool> SaveChangesAsync()
+        {
+            return (await _context.SaveChangesAsync()) > 0;
+        }
+
+        public async Task<Aluno[]> GetAllAlunosAsync(bool includeProfessor = false)
+        {
+            IQueryable<Aluno> query = _context.Alunos;
+
+            if (includeProfessor)
+            {
+                query = query.Include(pe => pe.AlunosDisciplinas)
+                             .ThenInclude(ad => ad.Disciplina);
+            }
+
+            query = query.AsNoTracking()
+                         .OrderBy(c => c.Id);
+
+            return await query.ToArrayAsync();
+        }
+        public async Task<Aluno> GetAlunoAsyncById(int alunoId, bool includeDisciplina)
+        {
+            IQueryable<Aluno> query = _context.Alunos;
+
+            if (includeDisciplina)
+            {
+                query = query.Include(a => a.AlunosDisciplinas)
+                             .ThenInclude(ad => ad.Disciplina)
+                             .ThenInclude(d => d.Professor);
+            }
+
+            query = query.AsNoTracking()
+                         .OrderBy(aluno => aluno.Id)
+                         .Where(aluno => aluno.Id == alunoId);
+
+            Aluno? aluno = await query.FirstOrDefaultAsync();
+            return aluno;
+        }
+        public async Task<Aluno[]> GetAlunosAsyncByDisciplinaId(int disciplinaId, bool includeDisciplina)
+        {
+            IQueryable<Aluno> query = _context.Alunos;
+
+            if (includeDisciplina)
+            {
+                query = query.Include(p => p.AlunosDisciplinas)
+                             .ThenInclude(ad => ad.Disciplina)
+                             .ThenInclude(d => d.Professor);
+            }
+
+            query = query.AsNoTracking()
+                         .OrderBy(aluno => aluno.Id)
+                         .Where(aluno => aluno.AlunosDisciplinas.Any(ad => ad.DisciplinaId == disciplinaId));
+
+            return await query.ToArrayAsync();
+        }
+
+        public async Task<Professor[]> GetProfessoresAsyncByAlunoId(int alunoId, bool includeDisciplina)
+        {
+            IQueryable<Professor> query = _context.Professores;
+
+            if (includeDisciplina)
+            {
+                query = query.Include(p => p.Disciplina);
+            }
+
+            query = query.AsNoTracking()
+                         .OrderBy(aluno => aluno.Id)
+                         .Where(aluno => aluno.Disciplina.Any(d =>
+                            d.AlunosDisciplinas.Any(ad => ad.AlunoId == alunoId)));
+
+            return await query.ToArrayAsync();
+        }
+
+        public async Task<Professor[]> GetAllProfessoresAsync(bool includeDisciplinas = true)
+        {
+            IQueryable<Professor> query = _context.Professores;
+
+            if (includeDisciplinas)
+            {
+                query = query.Include(c => c.Disciplina);
+            }
+
+            query = query.AsNoTracking()
+                         .OrderBy(professor => professor.Id);
+
+            return await query.ToArrayAsync();
+        }
+        public async Task<Professor> GetProfessorAsyncById(int professorId, bool includeDisciplinas = true)
+        {
+            IQueryable<Professor> query = _context.Professores;
+
+            if (includeDisciplinas)
+            {
+                query = query.Include(pe => pe.Disciplina);
+            }
+
+            query = query.AsNoTracking()
+                         .OrderBy(professor => professor.Id)
+                         .Where(professor => professor.Id == professorId);
+
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public Task<Aluno[]?> GetAlunoAsyncById()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Aluno[]?> GetAlunoAsyncById(bool v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+
